e6b0307976113df57d4fef67015107618407502f
saanvijay <mvjp.183@gmail.com>
2019-11-01 21:42:45 +0530

    Changed README and generalize the script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dynamicFabricNetworkOps
 Dynamically Add/Delete organizations to channel and add/delete organization to consortium in the running Hyperledger Fabric network
+
+There are 4 options available in this script
+	1. You can add 3rd Organization into the existing consortium i.e SampleConsortium
+	2. You can remove 3rd Organization from the consortium if it exists already in SampleConsortium
+	3. You can add 3rd Organization into the existing system channel ( not application channel )
+	4. You can remove 3rd Organization from the existing system channel ( not application channel )
--- fabric-network-ops.sh
+++ fabric-network-ops.sh
@@ -14,7 +14,7 @@ CORE_PEER_LOCALMSPID=OrdererMSP
 CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/tls/ca.crt
 
 
-peer channel fetch config config_block.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
+peer channel fetch config config_block.pb -c $CHANNEL_NAME -o $CORE_PEER_ADDRESS  --tls --cafile $ORDERER_CA
 
 configtxlator proto_decode --input config_block.pb --type common.Block | jq .data.data[0].payload.data.config > config.json
 
@@ -22,13 +22,13 @@ configtxlator proto_decode --input config_block.pb --type common.Block | jq .dat
 jq -s '.[0] * {"channel_group":{"groups":{"Consortiums":{"groups": {"SampleConsortium": {"groups": {"Org3MSP":.[1]}}}}}}}' config.json ./channel-artifacts/org3.json > modified_config.json
 
 #### Delete Org from Consortium ######
- cat config.json | jq "del(.channel_group.groups.Consortiums.groups.SampleConsortium.groups.Org3MSP)" > modified_config.json
+# cat config.json | jq "del(.channel_group.groups.Consortiums.groups.SampleConsortium.groups.Org3MSP)" > modified_config.json
 
 #### Add Organization to channel #####
-jq -s '.[0] * {"channel_group":{"groups":{"Application":{"groups": {"Org3MSP":.[1]}}}}}' config.json ./channel-artifacts/org3.json > modified_config.json
+#jq -s '.[0] * {"channel_group":{"groups":{"Application":{"groups": {"Org3MSP":.[1]}}}}}' config.json ./channel-artifacts/org3.json > modified_config.json
 
 #### Delete Oraganization from channel ####
-jq 'del(.channel_group.groups.Application.groups.Org3MSP)' config.json > modified_config.json
+#jq 'del(.channel_group.groups.Application.groups.Org3MSP)' config.json > modified_config.json
 
 
 configtxlator proto_encode --input config.json --type common.Config --output config.pb
@@ -39,11 +39,11 @@ configtxlator compute_update --channel_id $CHANNEL_NAME --original config.pb --u
 
 configtxlator proto_decode --input org3_update.pb --type common.ConfigUpdate | jq . > org3_update.json
 
-echo '{"payload":{"header":{"channel_header":{"channel_id":"byfn-sys-channel", "type":2}},"data":{"config_update":'$(cat org3_update.json)'}}}' | jq . > org3_update_in_envelope.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"$CHANNEL_NAME", "type":2}},"data":{"config_update":'$(cat org3_update.json)'}}}' | jq . > org3_update_in_envelope.json
 
 configtxlator proto_encode --input org3_update_in_envelope.json --type common.Envelope --output org3_update_in_envelope.pb
 
 
 peer channel signconfigtx -f org3_update_in_envelope.pb
 
-peer channel update -f org3_update_in_envelope.pb -c $CHANNEL_NAME -o orderer.example.com:7050 --tls --cafile $ORDERER_CA
+peer channel update -f org3_update_in_envelope.pb -c $CHANNEL_NAME -o $CORE_PEER_ADDRESS --tls --cafile $ORDERER_CA
